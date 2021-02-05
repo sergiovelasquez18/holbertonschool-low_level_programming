838d43bfbe1b1988ab6ff52dea5b1d986fed9114
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-04 22:48:54 -0500

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - imprints alphabet
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		putchar (letra);
+	}
+	putchar ('\n');
+return (0);
+}

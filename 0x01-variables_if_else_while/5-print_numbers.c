33ccd2ef43da5602846e1b31eeae5723f31f6b9b
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-05 23:25:20 -0500

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - imprints numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	printf("%d", l);
+	printf("\n");
+	return (0);
+}

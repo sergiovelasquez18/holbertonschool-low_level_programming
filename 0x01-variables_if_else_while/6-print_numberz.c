4e703e35c0604bbd40afc0de05dee104f25c5db8
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-05 23:44:14 -0500

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - imprints alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = '0'; v <= '9'; v++)
+	putchar(v);
+	putchar('\n');
+	return (0);
+	}

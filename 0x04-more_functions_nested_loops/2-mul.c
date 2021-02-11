4119b023ef94c510e96a3951337693b575735913
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-11 16:02:54 -0500

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers.
+ * @a: letter type
+ * @b: letter type
+ * Return: return 1 uppercase and 0 if not
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

8fc850655761afcd865239231d15eabf9d4f3cfa
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-16 11:41:31 -0500

    exchanges values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - exchanges the values
+ * @a: contains the value of the variable
+ * @b: contains the value of the variable
+ * Return: Always 0 (Succes)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

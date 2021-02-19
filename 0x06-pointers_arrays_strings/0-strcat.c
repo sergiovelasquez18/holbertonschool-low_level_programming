fe7efe5bda41610ee6115685eadd0cb6cd243669
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-18 21:14:34 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strcat - print the elements of an array
+ * @dest: contains the string
+ * @src: contains the string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int sel, g;
+
+	sel = 0;
+	while (dest[sel] != '\0')
+	{
+		++sel;
+	}
+	for (g = 0; src[g] != '\0'; ++g, ++sel)
+	{
+		dest[sel] = src[g];
+	}
+	dest[sel] = '\0';
+	return (dest);
+}

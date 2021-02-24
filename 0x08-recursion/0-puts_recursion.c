32971c522283dd089c872679b56fc70b6528fa67
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-24 16:50:41 -0500

    puts rescursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - funtion
+ * @s: string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}

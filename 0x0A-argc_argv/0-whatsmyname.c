7bc48d457a53027bcd0201af77456404456246c0
sergiovelasquez18 <2458@holbertonschool.com>
2021-02-28 20:21:50 -0500

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - print its name + \n
+ * @argc: number of argument type
+ * @argv: array pointing to aruguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}

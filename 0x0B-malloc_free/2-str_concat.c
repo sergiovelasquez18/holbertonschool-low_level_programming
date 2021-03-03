72658be914641f4cfd9ddcef123b003b35dae9b0
sergiovelasquez18 <2458@holbertonschool.com>
2021-03-03 00:23:00 -0500

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,9 @@ char *str_concat(char *s1, char *s2)
 	int t, g, size;
 	char *a;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
 	for (t = 0; s1[t] != '\0'; t++)
 		;
-	for (g = 0; s2[g] != '\0'; t++)
+	for (g = 0; s2[g] != '\0'; g++)
 		;
 	size = t + g + 1;
 	a = malloc(sizeof(char) * size);

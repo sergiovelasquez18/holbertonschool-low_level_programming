0e5949f3eebc3834e065f45fb52d9365e378c271
sergiovelasquez18 <2458@holbertonschool.com>
2021-03-23 00:15:21 -0500

    add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include "string.h"
+/**
+ * add_node - adds a new node at the beginning of list_t list
+ * @head: pointer to apointera to a struct
+ * @str: string to be copied
+ * Return: the addres of new element or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int i;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+		
+}

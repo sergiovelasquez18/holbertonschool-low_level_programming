dd3180ebb15b308cc259afc4c064eb91213b0dab
sergiovelasquez18 <2458@holbertonschool.com>
2021-03-24 22:30:21 -0500

    pop listing

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - freee a listint_t list
+ * @head: pointer a pointer to struct
+ * Return: the head node's data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	int tmp;
+	listint_t *headref;
+
+	if (*head == NULL)
+		return (0);
+	headref = (*head)->next;
+	tmp = (*head)->n;
+	free(*head);
+	*head = headref;
+	return (tmp);
+}

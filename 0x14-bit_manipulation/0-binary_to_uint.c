8254774412ba711f8577374b952d3237010f19ad
sergiovelasquez18 <2458@holbertonschool.com>
2021-03-26 23:31:25 -0500

    binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - coverts a binary numberan unsigne int
+ * @b: pointer to string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binary number, or 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int n;
+
+	n = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n <<= 1;
+		if (b[i] == '1')
+			n += 1;
+	}
+	return (n);
+}

3ecc38cc1825ebe66cac106744cfa338885eeb5d
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-31 16:38:01 -0500

    bookstore

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H_
+#define SEARCH_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
